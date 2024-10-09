ddf6ea9d2de759dfd0f06f047f45a33f663163e0
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2024-10-08 21:23:51 -0700

    fixed project accent not showing up on compact tasks

--- lib/widgets/task_item/compact_task.dart
+++ lib/widgets/task_item/compact_task.dart
@@ -24,6 +24,7 @@ class CompactTaskItem extends ConsumerWidget {
       TaskText(
         title: task.name.toString(),
         projectName: project?.name,
+        projectAccentColor: project?.color,
         useLongerProjectGap: true,
       ),
     ]);
