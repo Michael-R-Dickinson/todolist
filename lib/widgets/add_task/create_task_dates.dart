0a5380aaec58aceb6dd6344e3279f33381b46f32
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2024-10-15 19:19:05 -0700

    added small padding to top of day selectors

--- lib/widgets/add_task/create_task_dates.dart
+++ lib/widgets/add_task/create_task_dates.dart
@@ -124,6 +124,9 @@ class DayButtons extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [
+        SizedBox(
+          height: 4,
+        ),
         DayButton(
           label: "Today",
           iconColor: Colors.green,
